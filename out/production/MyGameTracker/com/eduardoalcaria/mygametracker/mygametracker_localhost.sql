0992211f4c823c39913e39d4ec8c82f3c3c924ad
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-03-01 20:53:53 -0300

    add see database feature and implemented insert data to database via a InsertGames class

---
+++ out/production/MyGameTracker/com/eduardoalcaria/mygametracker/mygametracker_localhost.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 28, 2025 at 01:10 AM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mygametracker_localhost`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+DROP TABLE IF EXISTS `games`;
+CREATE TABLE IF NOT EXISTS `games` (
+  `game_id` int NOT NULL AUTO_INCREMENT,
+  `game_title` varchar(255) DEFAULT NULL,
+  `game_genre` varchar(150) DEFAULT NULL,
+  `game_status` varchar(15) DEFAULT NULL,
+  `start_playing_on` date DEFAULT NULL,
+  `finished_playing_on` date DEFAULT NULL,
+  PRIMARY KEY (`game_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
