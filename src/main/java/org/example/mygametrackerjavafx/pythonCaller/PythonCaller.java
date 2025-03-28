3cf3767f29c2d955206f620b6375ebe30d21ae61
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-03-27 23:59:11 -0300

    add the python caller that calls the steamWebScrapper.py thought a processBuilder and then gives the game name as an argument

--- src/main/java/org/example/mygametrackerjavafx/pythonCaller/PythonCaller.java
+++ src/main/java/org/example/mygametrackerjavafx/pythonCaller/PythonCaller.java
@@ -3,9 +3,11 @@ package org.example.mygametrackerjavafx.pythonCaller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 public class PythonCaller {
     public static void main(String[] args) throws IOException {
+        Date date = new Date();
         try {
              ProcessBuilder processBuilder = new ProcessBuilder("python",
                      System.getProperty("user.dir") + "\\src\\main\\java\\org\\example\\mygametrackerjavafx\\python_mygametracker\\steamWebScrapper.py",
@@ -15,9 +17,10 @@ public class PythonCaller {
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
             String line=null;
-            while ((line=reader.readLine()) != null){
-                System.out.println("lines " + line);
+            while ((line=reader.readLine()) != null) {
+                    System.out.println(date + "  " + line);
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/org/example/mygametrackerjavafx/python_mygametracker/steamWebScrapper.py
+++ src/main/java/org/example/mygametrackerjavafx/python_mygametracker/steamWebScrapper.py
@@ -1,71 +1,32 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 import sys
-from time import sleep
-import random
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=options)
-driver.get("https://store.steampowered.com/")
 
 game_name = sys.argv[1].lower()
 
-
-def randomsleep():
-    rand = random.randint(0,3)
-    sleep(rand)
-
-def title_verifier(game_titles):
+def title_verifier_via_steam(game_titles):
     count = 0
     for game_title in game_titles:
-        if game_name.lower() in game_title.text.lower():
+        if game_name in game_title.text.lower():
             return True
         count += 1
         if count == 5:
             break
-
-
-
-def main():
-
-    search_box = driver.find_element(By.XPATH, "//*[@id='store_nav_search_term']")
-
-    randomsleep()
-
-
-    search_box.click()
-
-    randomsleep()
-
-    for l in game_name:
-        search_box.send_keys(l)
-        sleep(0.1)
-
-    sleep(1)
-
-    search_box.send_keys(Keys.ENTER)
-
-    randomsleep()
+def scrapper():
+    driver.get(f"https://store.steampowered.com/search/?term={game_name.lower().replace(' ', '+').strip().rstrip()}")
 
     html = urlopen(driver.current_url)
-
-    randomsleep()
 
     bs = BeautifulSoup(html, "html.parser")
 
     game_titles = bs.find_all("div", {"class": "col search_name ellipsis"})
-
-
-    print(title_verifier(game_titles))
-
-    randomsleep()
-main()
-
-
+    title_verified = title_verifier_via_steam(game_titles)
+    if title_verified:
+        print(f"{title_verified}\nsteam")
+scrapper()
